14a8ba0b8cba274531b707263ea58a68cec34668
Christi Harlow <cah120671@gmail.com>
2024-03-24 14:47:19 -0400

    Update Favorites.js

--- src/Home/Favorites.js
+++ src/Home/Favorites.js
@@ -12,7 +12,9 @@ const Favorites = () => {
                 window.location.host.indexOf("christisfavoritethings.com") !==
                 -1
             ) {
+                
                 host = "https://favorite-things-120671.ue.r.appspot.com";
+                console.log("Changing host to" , host);
             }
 
             try {
