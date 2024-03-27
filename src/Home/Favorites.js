b09141a7974cc1cd344e719d272b81d40136aa67
Christi Harlow <cah120671@gmail.com>
2024-03-27 13:35:16 -0400

    updates

--- src/Home/Favorites.js
+++ src/Home/Favorites.js
@@ -13,8 +13,7 @@ const Favorites = () => {
                 -1
             ) {
                 
-                host = "https://christisfavoritethings.com:3020";
-                console.log("Changing host to" , host);
+                host = "https://christisfavoritethings.com";
             }
 
             try {
