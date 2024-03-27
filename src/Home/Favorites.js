b2ff678c04ff230fabd2a1f4c0a3ab2fcf5f8ffa
Christi Harlow <cah120671@gmail.com>
2024-03-27 14:11:56 -0400

    Update Favorites.js

--- src/Home/Favorites.js
+++ src/Home/Favorites.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 //import { Link } from "react-router-dom";
+import Masonry from "masonry-layout"; // Import Masonry
 
 const Favorites = ({host}) => {
     const [favorites, setFavorites] = useState([]);
