9e6ddd6b60dff3551e454f9b584411ea8ad3d4b5
Christi Harlow <cah120671@gmail.com>
2024-03-28 12:08:52 -0400

    Update Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,8 +3,8 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 const Home = () => {
-    // Define the host URL
-    const host = "https://christisfavoritethings.com";
+    // Define the host URL for local deployment
+    const host = "http://localhost:3020";
 
     return (
         <div className="container">
