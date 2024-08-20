11896514c3e82ebef7530ec312bafa25a7669992
Christi Harlow <cah120671@gmail.com>
2024-08-20 17:57:47 -0400

    Add Home component with API URL and layout

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,21 +1,21 @@
-import Favorites from "./Favorites";
-import Footer from "./Footer";
-import Header from "./Header";
-
-
+import React from 'react';
+import Favorites from './Favorites';
+import Footer from './Footer';
+import Header from './Header';
 
 function Home() {
-    // Define the host URL for local deployment
-    const apiUrl = 'https://api.christisfavoritethings.com/api';
+  // Define the host URL for local deployment
+  const apiUrl = 'https://api.christisfavoritethings.com/api';
 
-
-    return (
-        <div className="container">
-            <Header />
-            <Favorites host={apiUrl} />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="container">
+      <Header />
+      <Favorites host={apiUrl} />
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
+
+
